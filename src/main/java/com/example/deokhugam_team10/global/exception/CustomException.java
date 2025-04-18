3cd71b3e6e0e4d5e6d1b3d08e0d41e33715ab967
dkseoals15 <aa88491231@gmail.com>
2025-04-18 09:10:21 +0900

    infra setup

---
+++ src/main/java/com/example/deokhugam_team10/global/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.deokhugam_team10.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
