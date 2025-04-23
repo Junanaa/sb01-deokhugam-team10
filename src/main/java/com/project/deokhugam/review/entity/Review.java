84934c781f1dc5aa6bdf99c669fd87ab9cd3e16d
dkseoals15 <aa88491231@gmail.com>
2025-04-23 15:33:42 +0900

    refactor: review 엔티티 ID 타입을 Long에서 UUID로 변경

--- src/main/java/com/project/deokhugam/review/entity/Review.java
+++ src/main/java/com/project/deokhugam/review/entity/Review.java
@@ -1,23 +1,18 @@
 package com.project.deokhugam.review.entity;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import com.project.deokhugam.book.entity.Book;
 import com.project.deokhugam.user.entity.User;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Getter
@@ -29,8 +24,10 @@ import lombok.Setter;
 public class Review {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long reviewId;
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "review_id", updatable = false, nullable = false)
+	private UUID reviewId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "book_id")
