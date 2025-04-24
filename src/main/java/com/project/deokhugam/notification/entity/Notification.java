bd8e5a79b7c48b60ce45063b9828e97873033648
gunhee <sb04214@naver.com>
2025-04-24 09:31:31 +0900

    refactor: 알림(Notification) 엔티티 클래스 코드 변경

--- src/main/java/com/project/deokhugam/notification/entity/Notification.java
+++ src/main/java/com/project/deokhugam/notification/entity/Notification.java
@@ -3,21 +3,12 @@ package com.project.deokhugam.notification.entity;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import com.project.deokhugam.user.entity.User;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Entity
 @Table(name = "notification")
@@ -29,16 +20,17 @@ import lombok.Setter;
 public class Notification {
 
     @Id
-    @Column(name = "notification_id", columnDefinition = "BINARY(16)")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "notification_id", updatable = false, nullable = false)
     private UUID id;
 
-    @Column(name = "review_id", nullable = false, columnDefinition = "BINARY(16)")
+    @Column(name = "review_id", nullable = false, columnDefinition = "uuid")
     private UUID reviewId;
 
-    @Column(name = "book_id", nullable = false, columnDefinition = "BINARY(16)")
+    @Column(name = "book_id", nullable = false, columnDefinition = "uuid")
     private UUID bookId;
 
-    @Column(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
+    @Column(name = "user_id", nullable = false, columnDefinition = "uuid")
     private UUID userId;
 
     @Column(name = "content", nullable = false, columnDefinition = "TEXT")
@@ -53,10 +45,6 @@ public class Notification {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    /**
-     * User.notifications(mappedBy="user") 와 매핑될 필드
-     * insertable=false, updatable=false 로 userId 필드를 우선 사용하도록 설정
-     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", insertable = false, updatable = false)
     private User user;
